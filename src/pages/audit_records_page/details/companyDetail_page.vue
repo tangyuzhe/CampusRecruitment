8389f177cbe25a5e4edf0f7c465949b45e0fc14d
tangyuzhe <www.twuwu@qq.com>
2020-09-23 20:09:49 +0800

    分页获取申请记录

---
+++ src/pages/audit_records_page/details/companyDetail_page.vue
@@ -0,0 +1,71 @@
+<template>
+  <view>
+    <u-table font-size="26">
+      <u-tr class="u-tr">
+        <u-th class="u-th">company公司</u-th>
+      </u-tr>
+      <u-tr class="u-tr">
+        <u-td class="u-td" width="30%">申请人</u-td>
+        <u-td class="u-td">吴佳豪</u-td>
+      </u-tr>
+      <u-tr class="u-tr">
+        <u-td class="u-td" width="30%">所在地</u-td>
+        <u-td class="u-td">中国江苏省苏州市吴江区</u-td>
+      </u-tr>
+      <u-tr class="u-tr">
+        <u-td class="u-td" width="30%">企业性质</u-td>
+        <u-td class="u-td">国企</u-td>
+      </u-tr>
+      <u-tr class="u-tr">
+        <u-td class="u-td" width="30%">企业规模</u-td>
+        <u-td class="u-td">1000-5000人</u-td>
+      </u-tr>
+      <u-tr class="u-tr">
+        <u-td width="30%">简介</u-td>
+        <u-td class="u-td">他他他他他他他他他他他他他他他他他他他他他他他他他他他他他他他他他他他他他他他他他他他他他他他他他他他他他他</u-td>
+      </u-tr>
+      <u-tr class="u-tr">
+        <u-td class="u-td" width="30%">企业官网</u-td>
+        <u-td class="u-td">www.baidu.com</u-td>
+      </u-tr>
+      <u-tr class="u-tr">
+        <u-td class="u-td" width="30%">招聘网站</u-td>
+        <u-td class="u-td">www.baidu.com</u-td>
+      </u-tr>
+      <u-tr class="u-tr">
+        <u-td class="u-td" width="30%">营业执照</u-td>
+        <u-td class="u-td">
+          <u-image src="https://cdn.uviewui.com/uview/example/fade.jpg" mode="widthFix"></u-image>
+        </u-td>
+      </u-tr>
+      <u-tr class="u-tr">
+        <u-td class="u-td" width="30%">申请时间</u-td>
+        <u-td class="u-td">2020年12月21日 12:30:42</u-td>
+      </u-tr>
+      <u-tr style="border:none">
+        <u-td>
+          <u-button type="error" size="mini">不通过</u-button>
+        </u-td>
+        <u-td>
+          <u-button type="primary" size="mini">通过</u-button>
+        </u-td>
+      </u-tr>
+    </u-table>
+  </view>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import * as api from "../../../api/request";
+@Component({
+  components: {},
+})
+export default class CompanyDetailPage extends Vue {
+  onLoad(option: any) {
+    console.log(option.company_id);
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
--- src/pages/audit_records_page/recordsTable/applicationRecords.vue
+++ src/pages/audit_records_page/recordsTable/applicationRecords.vue
@@ -1,29 +1,80 @@
 <template>
   <view>
-    <u-table>
-      <u-tr class="u-tr">
-        <u-th class="u-th">姓名</u-th>
-        <u-th class="u-th">年龄</u-th>
-        <u-th class="u-th">籍贯</u-th>
-        <u-th class="u-th">性别</u-th>
+    <u-table font-size="28" align="left">
+      <u-tr class="u-tr" style="height:40px">
+        <u-th class="u-th" width="30%">序号</u-th>
+        <u-th class="u-th" width="50%">公司名</u-th>
+        <u-th class="u-th" width="20%">查看详情</u-th>
       </u-tr>
-      <u-tr class="u-tr">
-        <u-td class="u-td">吕布</u-td>
-        <u-td class="u-td">22</u-td>
-        <u-td class="u-td">楚河</u-td>
-        <u-td class="u-td">男</u-td>
+      <u-tr class="u-tr" v-for="(item,index) in list" :key="index" style="height:50px">
+        <u-td class="u-td" width="30%">{{index+1}}</u-td>
+        <u-td class="u-td" width="50%">{{item.company}}</u-td>
+        <u-td class="u-td" width="20%">
+          <u-button type="primary" size="mini" @click="WatchCompanyDetail(item.id)">查看</u-button>
+        </u-td>
       </u-tr>
     </u-table>
+    <van-pagination
+      v-model="currentPage"
+      :page-count="pageCount"
+      mode="simple"
+      @change="paginationChange"
+    />
   </view>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import * as api from "../../../api/request";
+import { Pagination } from "vant";
 @Component({
-  components: {},
+  components: {
+    Pagination,
+  },
 })
-export default class ApplicationRecordsTable extends Vue {}
+export default class ApplicationRecordsTable extends Vue {
+  currentPage: number = 1;
+  pageCount: number = 1;
+  list: any = [];
+  params: any = {
+    currentPage: 1,
+    pageSize: 15,
+  };
+
+  //获取申请列表
+  async GetApplicationList() {
+    await api.BaseRequest.getRequest("/v1/application?", this.params).then(
+      (res: any) => {
+        if (res.data.code == 0) {
+          this.pageCount = Math.ceil(
+            res.data.data.count / this.params.pageSize
+          );
+          this.list = res.data.data.rows;
+        }
+      }
+    );
+  }
+
+  //当前页改变
+  paginationChange(val: number) {
+    this.params.currentPage = val;
+    this.GetApplicationList();
+  }
+
+  //查看公司详情
+  WatchCompanyDetail(val: number) {
+    console.log(val);
+    uni.navigateTo({
+      url:
+        "/pages/audit_records_page/details/companyDetail_page?company_id=" +
+        val,
+    });
+  }
+
+  created() {
+    this.GetApplicationList();
+  }
+}
 </script>
 
 <style scoped lang="scss">
